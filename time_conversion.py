07a9d3a3df8d5b714c1b517e2a6df1c2d383fed1
shivkondraju <82672285+shivkondraju@users.noreply.github.com>
2021-04-17 22:06:50 +0530

    Create time_conversion.py
    This code reads the ticker data csv and converts the time which is in micro second numeric format to readable date-time format

---
+++ time_conversion.py
@@ -0,0 +1,19 @@
+#Importing Pandas and Datetime
+import pandas as pd
+from datetime import datetime
+
+#Reading Ticker data CSV file
+data=pd.read_csv(r"tick_20200302.csv")
+data.shape
+data.head()
+
+#Creating a copy
+data_eg=data
+
+#data_time is the new column to host the new updated time
+data_eg['data_time']=data_eg['Date'].astype(str)+data_eg['Time'].astype(str)
+data_eg['data time']=pd.to_datetime(data_eg['data_time'],format='%Y%m%d%H%M%S%f')
+data_eg['data time']=data_eg['data time'].values.astype('datetime64[s]')
+
+#Replacing data of Date column with the updated date time
+data_eg['Date']=data_eg['data time']
